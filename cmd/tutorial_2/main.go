8de188cb5cc65291c5e0da6b1cdc42ccfa060fbb
Barrett <barrettkowalsky@gmail.com>
2025-07-07 09:24:25 -0600

    slices and capacities

--- cmd/tutorial_2/main.go
+++ cmd/tutorial_2/main.go
@@ -3,26 +3,21 @@ package main
 import "fmt"
 
 func main() {
-	// [0,0,0]
-	var intArr [3]int32
-	// Because int32 = 4 bytes of mem, and there are 3 int32s,
-	// go initializes array with 12 bytes of contiguous memory
-	intArr[0] = 1
-	intArr[1] = 123
-	intArr[2] = 666
-	fmt.Println(intArr[0])
-	fmt.Println(intArr[1:3])
+	intArr := [...]int32{1, 2, 3}
+	fmt.Println("intArr:", intArr)
 
-	// Printing location in mem of each element
-	fmt.Println(&intArr[0])
-	fmt.Println(&intArr[1])
-	fmt.Println(&intArr[2])
+	// Appending + checking the capacity of the slice
+	var intSlice []int32 = []int32{4, 5, 6}
+	fmt.Printf("The length of the slice is %v with capacity %v", len(intSlice), cap(intSlice))
+	intSlice = append(intSlice, 7)
+	fmt.Printf("\nThe NEW length of the slice is %v with NEW capacity %v\n", len(intSlice), cap(intSlice))
+	// ^^ Capacity of the slice becomes 6
 
-	// Shorthand init
-	intArr2 := [3]int32{1, 2, 3}
-	fmt.Println(intArr2)
+	var intSlice2 []int32 = []int32{8, 9}
+	intSlice = append(intSlice, intSlice2...)
+	fmt.Println(intSlice)
 
-	// Or can use [...] which will imply the length of the array based on total elements
-	intArr3 := [...]int32{4,5,6}
-	fmt.Println(intArr3)
+	var intSlice3 []int32 = make([]int32, 3, 8)
+	fmt.Println("Length: ", len(intSlice3))
+	fmt.Println("Capacity: ", cap(intSlice3))
 }
