17f636da505dc18a50a4f8b10daa62e9ebd72aed
Barrett <barrettkowalsky@gmail.com>
2025-07-07 08:51:07 -0600

    arr overview

--- cmd/tutorial_2/main.go
+++ cmd/tutorial_2/main.go
@@ -17,4 +17,12 @@ func main() {
 	fmt.Println(&intArr[0])
 	fmt.Println(&intArr[1])
 	fmt.Println(&intArr[2])
+
+	// Shorthand init
+	intArr2 := [3]int32{1, 2, 3}
+	fmt.Println(intArr2)
+
+	// Or can use [...] which will imply the length of the array based on total elements
+	intArr3 := [...]int32{4,5,6}
+	fmt.Println(intArr3)
 }
