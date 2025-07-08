9bd3777e6671e1c9e2e79bc07423d13a6b405973
Barrett <barrettkowalsky@gmail.com>
2025-07-08 09:01:55 -0600

    loopies

--- cmd/tutorial_3_maps/main.go
+++ cmd/tutorial_3_maps/main.go
@@ -12,10 +12,44 @@ func main() {
 	// A map will always return something, even if a key does not exist
 	fmt.Print("\n", myMap2["Jason"])
 	var age, ok = myMap2["Jason"]
-	delete(myMap2, "Adam")
+	// delete(myMap2, "Adam")
 	if ok {
 		fmt.Printf("The age is %v", age)
 	} else {
 		fmt.Println("Invalid Name")
+	}
+
+	for name, age := range myMap2 {
+		fmt.Printf("Name: %v, Age: %v\n", name, age)
+	}
+
+	intSlice := []int32{4, 5, 6}
+
+	// Iterating over arrays and slices
+	for i, v := range intSlice {
+		fmt.Printf("Index: %v, Value: %v \n", i, v)
+	}
+
+	// "While" loops in go - bit different!
+	var i int = 0
+	for i < 10 {
+		fmt.Println(i)
+		i = i + 1
+	}
+
+	// We can also eliminate the condition
+
+	var j int = 0
+	for {
+		if j >= 10 {
+			break
+		}
+		fmt.Println(j)
+		j = j + 1
+	}
+
+	// OR
+	for k := 0; k < 10; k++ {
+		fmt.Println(k)
 	}
 }
