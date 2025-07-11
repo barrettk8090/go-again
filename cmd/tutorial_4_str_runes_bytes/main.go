146925da9181867b7b4e3890d7057e0c477fad66
Barrett <barrettkowalsky@gmail.com>
2025-07-11 08:12:56 -0600

    rune v string

--- cmd/tutorial_4_str_runes_bytes/main.go
+++ cmd/tutorial_4_str_runes_bytes/main.go
@@ -3,13 +3,17 @@ package main
 import "fmt"
 
 func main() {
-	var myString = "résumé"
-	var indexed = myString[0]
+	var myString = []rune("résumé")
+	var indexed = myString[1]
 	// %v value, %T type
 	fmt.Printf("%v, %T\n", indexed, indexed)
 	for i, v := range myString {
 		fmt.Println(i, v)
 	}
 
-	
+	myRune := 'a'
+	myNewString := "a"
+	fmt.Printf("\n%v, %T", myRune, myRune)
+	fmt.Printf("\n%v, %T", myNewString, myNewString)
+
 }
